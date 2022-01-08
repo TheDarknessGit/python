0d3d640adc76e131b07fdd652ccd842c94e25ca4
TheDarknessGit <dzwarfield@gmail.com>
2022-01-08 19:34:36 +0000

    Remove comments and change the long & lat

--- fourthWeek/json_example.py
+++ fourthWeek/json_example.py
@@ -1,13 +1,6 @@
 import json, urllib2
 
-response = urllib2.urlopen("http://data.police.uk/api/crimes-street/all-crime?lat=52.587846&lng=-2.126226&date=2017-01")
-
-
-#woodside http://data.police.uk/api/crimes-street/all-crime?lat=52.651725&lng=-2.485432&date=2017-01
-#home http://data.police.uk/api/crimes-street/all-crime?lat=52.6861541&lng=-2.4676499&date=2017-01
-
-#print response.read()
-#response = open(nameofFile, read/write)
+response = urllib2.urlopen("http://data.police.uk/api/crimes-street/all-crime?lat=52.137837&lng=-0.898248&date=2017-01")
 
 crimes = json.loads(response.read())
 
